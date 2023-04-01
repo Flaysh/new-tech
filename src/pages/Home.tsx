b01fea2f0b574dd9df2ce50515d6cd29b9ffa4cc
Flaysh <flaysh@pm.me>
2023-04-01 17:29:49 +0300

    init project - first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+import type { ReactElement } from 'react'
+
+export default function Home(): ReactElement {
+	return (
+		<div className='flex min-h-screen items-center justify-center'>Home</div>
+	)
+}
